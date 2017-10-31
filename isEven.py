fc4a7f3186c76ace2487430f1536e612934fe4bd
Shaun Stute <shaunstute@gmail.com>
2017-10-31 14:46:34 -0500

    Add files via upload
    topk3k

---
+++ isEven.py
@@ -0,0 +1,12 @@
+def isEven(n):
+    import xml.etree.ElementTree as ET
+    import requests
+    question = "is {} even".format(n)
+    response = requests.get("http://api.wolframalpha.com/v2/query?input={}&appid={}".format(question, "secret"))
+    result = ET.fromstring(response.text).findall("./pod[@id='Result']")[0].find("subpod").find("plaintext").text
+    if "not" not in result.split():
+        return True
+        print(" is even")
+    else:
+        return False
+        print(" is false")
